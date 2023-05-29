a0574d89756db620419e44a882198b00a62496f0
Ayoub Elmortaji <88349298+AyoubElmortaji@users.noreply.github.com>
2023-05-29 23:07:13 +0100

    Update Heroimg.js

--- src/components/Heroimg.js
+++ src/components/Heroimg.js
@@ -7,7 +7,7 @@ const Heroimg = () => {
     <div className="hero">
         <div className="mask">
             <img className="intro-img" 
-            src="https://images.unsplash.com/photo-1523800503107-5bc3ba2a6f81?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2dyYW1taW5nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="intro-img"/>
+            src="https://images.unsplash.com/photo-1607743386760-88ac62b89b8a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAyfHxjb2Rpbmd8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="intro-img"/>
         </div>
 
         <div className="content">
